25d3c9628aa188af899a3002ac073e090daa72e0
Jon Graf <jongraf1@gmail.com>
2019-09-15 10:41:36 -0500

    updated route to use placeholders for simplification

--- svc/utilities/api_requests.py
+++ svc/utilities/api_requests.py
@@ -3,9 +3,11 @@ from datetime import datetime
 
 import requests
 
+POST_SUMP_CURRENT = 'http://localhost:8080/sumpPump/user/{}/currentDepth'
+
 
 def save_current_daily_depth(user_id, depth, time):
-    url = 'http://localhost:8080/sumpPump/user/' + user_id + '/currentDepth'
+    url = POST_SUMP_CURRENT.format(user_id)
     date = datetime.fromtimestamp(time)
     post_body = {'depth': depth, 'datetime': str(date)}
 
