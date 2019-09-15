6282c5111291ec2e3bec50a1d89a2d60bf6d2be0
Jon Graf <jongraf1@gmail.com>
2019-09-15 10:26:59 -0500

    added placeholder in controller for the user id

--- svc/controllers/controller.py
+++ svc/controllers/controller.py
@@ -2,11 +2,11 @@ from svc.utilities.gpio import get_intervals
 from svc.utilities.depth import get_depth_by_intervals
 from svc.utilities.api_requests import save_current_daily_depth
 
-
+USER_ID = None
 # TODO: Need to make api call to save current depth
 # TODO: Make into class to keep a running average of current depth
 def measure_depth():
     start, stop = get_intervals()
     depth = get_depth_by_intervals(start, stop)
-    save_current_daily_depth(depth, stop)
+    save_current_daily_depth(USER_ID, depth, stop)
     return depth
