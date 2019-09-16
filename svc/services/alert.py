530f253f2f9ab79e5ef9777daaf8db3f2db6f980
Jon Graf <jgraf@leantechniques.com>
2019-09-15 20:31:56 -0500

    will now send alert when depth greater than 40 percent over daily average

--- svc/services/alert.py
+++ svc/services/alert.py
@@ -3,12 +3,18 @@ from svc.services.api_requests import send_alert, send_warning
 
 EMERGENCY_DEPTH = 15.0
 WARNING_PERCENT = 0.2
+ALERT_PERCENT = 0.4
 
 
 def alert_validation(depth, daily_average, running_average):
+    percent_of_daily = round(1 - (daily_average / depth), 2)
+    percent_of_running = round(1 - (running_average / depth), 2)
+
     if depth <= EMERGENCY_DEPTH:
         send_alert()
-    elif round(1 - (daily_average / depth), 2) >= WARNING_PERCENT:
+    elif percent_of_daily > ALERT_PERCENT:
+        send_alert()
+    elif percent_of_daily >= WARNING_PERCENT:
         send_warning()
-    elif round(1 - (running_average / depth), 2) >= WARNING_PERCENT:
+    elif percent_of_running >= WARNING_PERCENT:
         send_warning()
--- test/unit/services/test_alert.py
+++ test/unit/services/test_alert.py
@@ -7,7 +7,7 @@ from svc.services.alert import alert_validation
 def test_alert_validation__should_call_send_alert_when_exceeds_emergency_threshold(mock_alert):
     depth = 14.0
     daily_average = 674.23
-    alert_validation(depth, daily_average, None)
+    alert_validation(depth, daily_average, 1000.0)
 
     mock_alert.assert_called()
 
@@ -16,7 +16,16 @@ def test_alert_validation__should_call_send_alert_when_exceeds_emergency_thresho
 def test_alert_validation__should_send_alert_when_distance_equal_emergency_threshold(mock_alert):
     depth = 15.0
     daily_average = 637.23
-    alert_validation(depth, daily_average, None)
+    alert_validation(depth, daily_average, 1000.0)
+
+    mock_alert.assert_called()
+
+
+@patch('svc.services.alert.send_alert')
+def test_alert_validation__should_send_alert_when_distance_greater_than_alert_percent(mock_alert):
+    depth = 200.0
+    daily_average = 119.0
+    alert_validation(depth, daily_average, 1000.0)
 
     mock_alert.assert_called()
 
@@ -25,7 +34,7 @@ def test_alert_validation__should_send_alert_when_distance_equal_emergency_thres
 def test_alert_validation__should_send_warning_when_depth_greater_than_twenty_percent_over_daily_average(mock_warning):
     depth = 200.0
     daily_average = 159.0
-    alert_validation(depth, daily_average, None)
+    alert_validation(depth, daily_average, 1000.0)
 
     mock_warning.assert_called()
 
@@ -34,7 +43,7 @@ def test_alert_validation__should_send_warning_when_depth_greater_than_twenty_pe
 def test_alert_validation__should_send_warning_when_depth_equal_twenty_percent_over_daily_average(mock_warning):
     depth = 200.0
     daily_average = 160.0
-    alert_validation(depth, daily_average, None)
+    alert_validation(depth, daily_average, 1000.0)
 
     mock_warning.assert_called()
 
@@ -43,7 +52,7 @@ def test_alert_validation__should_send_warning_when_depth_equal_twenty_percent_o
 def test_alert_validation__should_not_send_warning_when_fail_multiple_checks(mock_warning):
     depth = 15.0
     daily_average = 10.0
-    alert_validation(depth, daily_average, None)
+    alert_validation(depth, daily_average, 1000.0)
 
     assert mock_warning.call_count == 0
 
@@ -78,12 +87,14 @@ def test_alert_validation__should_not_send_warning_when_distance_above_emergency
     assert not mock_warning.called
 
 
-@patch('svc.services.alert.send_alert')
-def test_alert_validation__should_not_send_alert_when_depth_less_than_twenty_percent_greater_than_daily_average(
-        mock_alert):
+@patch('svc.services.alert.send_warning')
+def test_alert_validation__should_not_send_warning_when_depth_less_than_twenty_percent_greater_than_daily_average(
+        mock_warning):
     depth = 200.0
     daily_average = 180.0
     running_average = 1000.0
     alert_validation(depth, daily_average, running_average)
 
-    assert not mock_alert.called
+    assert not mock_warning.called
+
+
