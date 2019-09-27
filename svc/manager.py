8a60a150ffd3687e6e0eec786f49ef889d1bf05c
Jon Graf <jongraf1@gmail.com>
2019-09-27 16:31:18 -0500

    manager will now be passed the stop event flags

--- svc/manager.py
+++ svc/manager.py
@@ -1,13 +1,8 @@
-from threading import Event
-
 from svc.controllers.controller import DepthController
 from svc.services.event import MyThread
 
 
-def create_app():
-    interval_stop_flag = Event()
-    daily_stop_flag = Event()
-
+def create_app(interval_stop_flag, daily_stop_flag):
     controller = DepthController()
     interval_thread = MyThread(interval_stop_flag, controller.measure_depth, 120)
     interval_thread.start()
--- test/unit/test_manager.py
+++ test/unit/test_manager.py
@@ -1,4 +1,6 @@
-from mock import patch, ANY
+from threading import Event
+
+from mock import patch
 
 from svc.manager import create_app
 
@@ -6,29 +8,31 @@ from svc.manager import create_app
 @patch('svc.manager.MyThread')
 @patch('svc.manager.DepthController')
 class TestManager:
+    interval_thread = Event()
+    daily_thread = Event()
 
     def test_create_app__should_create_controller(self, mock_controller, mock_thread):
-        create_app()
+        create_app(self.interval_thread, self.daily_thread)
 
         mock_controller.assert_called()
 
     def test_create_app__should_create_interval_thread(self, mock_controller, mock_thread):
-        create_app()
+        create_app(self.interval_thread, self.daily_thread)
 
-        mock_thread.assert_any_call(ANY, mock_controller.return_value.measure_depth, 120)
+        mock_thread.assert_any_call(self.interval_thread, mock_controller.return_value.measure_depth, 120)
 
     def test_create_app__should_start_interval_thread(self, mock_controller, mock_thread):
-        create_app()
+        create_app(self.interval_thread, self.daily_thread)
 
         mock_thread.return_value.start.assert_called()
 
     def test_create_app__should_create_daily_thread(self, mock_controller, mock_thread):
-        create_app()
+        create_app(self.interval_thread, self.daily_thread)
 
-        mock_thread.assert_any_call(ANY, mock_controller.return_value.save_daily_average, 720)
+        mock_thread.assert_any_call(self.daily_thread, mock_controller.return_value.save_daily_average, 720)
 
     def test_create_app__should_start_daily_thread(self, mock_controller, mock_thread):
-        create_app()
+        create_app(self.interval_thread, self.daily_thread)
 
         mock_thread.return_value.start.assert_called()
         assert mock_thread.return_value.start.call_count == 2
