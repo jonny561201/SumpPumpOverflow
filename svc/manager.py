9aff01f5ea04783819ef3de68f240ef1137516b6
Jon Graf <jongraf1@gmail.com>
2019-09-27 16:10:39 -0500

    manager will now schedule the second thread to call the daily save service

--- svc/manager.py
+++ svc/manager.py
@@ -6,6 +6,10 @@ from svc.services.event import MyThread
 
 def create_app():
     interval_stop_flag = Event()
+    daily_stop_flag = Event()
+
     controller = DepthController()
     interval_thread = MyThread(interval_stop_flag, controller.measure_depth, 120)
     interval_thread.start()
+
+    MyThread(daily_stop_flag, controller.save_daily_average, 720)
--- test/unit/test_manager.py
+++ test/unit/test_manager.py
@@ -12,12 +12,17 @@ class TestManager:
 
         mock_controller.assert_called()
 
-    def test_create_app__should_create_daily_interval_thread(self, mock_controller, mock_thread):
+    def test_create_app__should_create_interval_thread(self, mock_controller, mock_thread):
         create_app()
 
-        mock_thread.assert_called_with(ANY, mock_controller.return_value.measure_depth, 120)
+        mock_thread.assert_any_call(ANY, mock_controller.return_value.measure_depth, 120)
 
-    def test_create_app__should_start_daily_interval_thread(self, mock_controller, mock_thread):
+    def test_create_app__should_start_interval_thread(self, mock_controller, mock_thread):
         create_app()
 
         mock_thread.return_value.start.assert_called()
+
+    def test_create_app__should_create_daily_thread(self, mock_controller, mock_thread):
+        create_app()
+
+        mock_thread.assert_any_call(ANY, mock_controller.return_value.save_daily_average, 720)
