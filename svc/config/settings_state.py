20b56ca4a2dfb097b500f7232bf6c0aca0ad9db9
Jon Graf <jgraf@vermeer.com>
2026-04-15 14:04:06 -0500

    updated the settings property to be better defined as I need to save the base url, port, and api key

--- svc/config/settings_state.py
+++ svc/config/settings_state.py
@@ -17,8 +17,8 @@ class Settings:
         return self._settings.get('Environment', 'local') if self._settings is not None else 'local'
 
     @property
-    def api_key_file(self):
-        return self._settings.get('ApiKeyFile') if self._settings is not None else 'apiKey.json'
+    def hub_info_file(self):
+        return self._settings.get('HubInfoFile') if self._settings is not None else 'apiKey.json'
 
     def __load_settings(self):
         try:
