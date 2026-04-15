340b4424aaad8b11c10ecffab92fc0ac6c4f5f5b
Jon Graf <jgraf@vermeer.com>
2026-04-15 14:24:55 -0500

    following python convention for app start up with if __main__

--- app.py
+++ app.py
@@ -7,10 +7,12 @@ logging.basicConfig(filename='sumpPump.log', level=logging.DEBUG)
 interval_stop_flag = Event()
 daily_stop_flag = Event()
 
-try:
-    logging.info('Application started!')
-    create_app(interval_stop_flag, daily_stop_flag)
-except KeyboardInterrupt:
-    interval_stop_flag.set()
-    daily_stop_flag.set()
-    logging.error('Application interrupted by user')
+
+if __name__ == '__main__':
+    try:
+        logging.info('Application started!')
+        create_app(interval_stop_flag, daily_stop_flag)
+    except KeyboardInterrupt:
+        interval_stop_flag.set()
+        daily_stop_flag.set()
+        logging.error('Application interrupted by user')
