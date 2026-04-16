a5fb0b8cccaa1905e990b4c990e48a1192311946
Jon Graf <jgraf@vermeer.com>
2026-04-16 10:36:37 -0500

    updated the calculate alert to no longer send warnings/alerts and backfill missing tests

--- svc/services/alert.py
+++ svc/services/alert.py
@@ -1,6 +1,3 @@
-from svc.services.api_requests import send_alert, send_warning
-
-
 EMERGENCY_DEPTH = 15.0
 WARNING_PERCENT = 1.2
 ALERT_PERCENT = 1.4
@@ -12,13 +9,10 @@ def calculate_alert(depth, daily_average, running_average):
     percent_of_running = round(running_average / depth, 2)
 
     if depth <= EMERGENCY_DEPTH:
-        send_alert()
         alert_level = 3
     elif (percent_of_daily >= ALERT_PERCENT) or (percent_of_running >= ALERT_PERCENT):
-        send_alert()
         alert_level = 2
     elif (percent_of_daily >= WARNING_PERCENT) or (percent_of_running >= WARNING_PERCENT):
-        send_warning()
         alert_level = 1
 
     return alert_level
--- test/unit/services/test_alert.py
+++ test/unit/services/test_alert.py
@@ -3,132 +3,7 @@ from mock import patch
 from svc.services.alert import calculate_alert
 
 
-@patch('svc.services.alert.send_alert')
-def test_calculate_alert__should_send_alert_when_exceeds_emergency_threshold(mock_alert):
-    depth = 14.0
-    daily_average = 674.23
-    calculate_alert(depth, daily_average, 1000.0)
-
-    mock_alert.assert_called()
-
-
-@patch('svc.services.alert.send_alert')
-def test_calculate_alert__should_send_alert_when_distance_equal_emergency_threshold(mock_alert):
-    depth = 15.0
-    daily_average = 637.23
-    calculate_alert(depth, daily_average, 1000.0)
-
-    mock_alert.assert_called()
-
-
-@patch('svc.services.alert.send_alert')
-def test_calculate_alert__should_send_alert_when_distance_greater_than_alert_percent_for_daily(mock_alert):
-    depth = 70.0
-    daily_average = 100.0
-    running_average = 70.0
-    calculate_alert(depth, daily_average, running_average)
-
-    mock_alert.assert_called()
-
-
-@patch('svc.services.alert.send_alert')
-def test_calculate_alert__should_send_alert_when_distance_equal_alert_percent_for_daily(mock_alert):
-    depth = 143.0
-    daily_average = 200.0
-    calculate_alert(depth, daily_average, 140.0)
-
-    mock_alert.assert_called()
-
-
-@patch('svc.services.alert.send_alert')
-def test_calculate_alert__should_send_alert_when_distance_greater_than_alert_percent_for_running(mock_alert):
-    depth = 140.0
-    daily_average = 140.0
-    running_average = 200.0
-    calculate_alert(depth, daily_average, running_average)
-
-    mock_alert.assert_called()
-
-
-@patch('svc.services.alert.send_alert')
-def test_calculate_alert__should_send_alert_when_distance_equal_alert_percent_for_running(mock_alert):
-    depth = 143.0
-    daily_average = 143.0
-    running_average = 200.0
-    calculate_alert(depth, daily_average, running_average)
-
-    mock_alert.assert_called()
-
-
-@patch('svc.services.alert.send_warning')
-def test_calculate_alert__should_send_warning_when_depth_greater_than_twenty_percent_over_daily_average(mock_warning):
-    depth = 160.0
-    daily_average = 200.0
-    calculate_alert(depth, daily_average, 160.0)
-
-    mock_warning.assert_called()
-
-
-@patch('svc.services.alert.send_warning')
-def test_calculate_alert__should_send_warning_when_depth_equal_twenty_percent_over_daily_average(mock_warning):
-    depth = 166.0
-    daily_average = 200.0
-    calculate_alert(depth, daily_average, 166.0)
-
-    mock_warning.assert_called()
-
-
-@patch('svc.services.alert.send_warning')
-def test_calculate_alert__should_send_warning_when_depth_greater_than_twenty_percent_greater_than_running_average(mock_warning):
-    depth = 160.0
-    daily_average = 160.0
-    running_average = 200.0
-    calculate_alert(depth, daily_average, running_average)
-
-    mock_warning.assert_called()
-
-
-@patch('svc.services.alert.send_warning')
-def test_calculate_alert__should_send_warning_when_depth_twenty_percent_greater_than_running_average(mock_warning):
-    depth = 166.0
-    daily_average = 166.0
-    running_average = 200.0
-    calculate_alert(depth, daily_average, running_average)
-
-    mock_warning.assert_called()
-
-
-@patch('svc.services.alert.send_warning')
-def test_calculate_alert__should_not_send_warning_when_fail_multiple_checks(mock_warning):
-    depth = 15.0
-    daily_average = 40.0
-    calculate_alert(depth, daily_average, 40.0)
-
-    assert mock_warning.call_count == 0
-
-
-@patch('svc.services.alert.send_warning')
-def test_calculate_alert__should_not_send_warning_when_distance_above_emergency_threshold(mock_warning):
-    depth = 600.0
-    daily_average = 500.0
-    running_average = 500.0
-    calculate_alert(depth, daily_average, running_average)
-
-    assert not mock_warning.called
-
-
-@patch('svc.services.alert.send_warning')
-def test_calculate_alert__should_not_send_warning_when_depth_greater_than_twenty_percent_above_daily_average(mock_warning):
-    depth = 170.0
-    daily_average = 200.0
-    running_average = 170.0
-    calculate_alert(depth, daily_average, running_average)
-
-    assert not mock_warning.called
-
-
-@patch('svc.services.alert.send_alert')
-def test_calculate_alert__should_return_warning_level_of_three_when_emergency(mock_alert):
+def test_calculate_alert__should_return_warning_level_of_three_when_below_threshold():
     depth = 14.0
     daily_average = 100.0
     actual = calculate_alert(depth, daily_average, 100.0)
@@ -136,8 +11,15 @@ def test_calculate_alert__should_return_warning_level_of_three_when_emergency(mo
     assert actual == 3
 
 
-@patch('svc.services.alert.send_alert')
-def test_calculate_alert__should_return_warning_level_of_two_when_alert_for_daily(mock_alert):
+def test_calculate_alert__should_return_warning_level_of_three_when_equal_to_threshold():
+    depth = 15.0
+    daily_average = 100.0
+    actual = calculate_alert(depth, daily_average, 100.0)
+
+    assert actual == 3
+
+
+def test_calculate_alert__should_return_warning_level_of_two_when_alert_for_daily():
     depth = 60.0
     daily_average = 100.0
     actual = calculate_alert(depth, daily_average, 60.0)
@@ -145,8 +27,23 @@ def test_calculate_alert__should_return_warning_level_of_two_when_alert_for_dail
     assert actual == 2
 
 
-@patch('svc.services.alert.send_alert')
-def test_calculate_alert__should_return_warning_level_of_two_when_alert_for_running(mock_alert):
+def test_calculate_alert__should_return_warning_level_of_two_when_daily_equal_alert_threshold():
+    depth = 60
+    daily_average = 84
+    actual = calculate_alert(depth, daily_average, 60.0)
+
+    assert actual == 2
+
+
+def test_calculate_alert__should_return_warning_level_of_two_when_running_equal_alert_threshold():
+    depth = 42.857
+    daily_average = 84
+    actual = calculate_alert(depth, daily_average, 60.0)
+
+    assert actual == 2
+
+
+def test_calculate_alert__should_return_warning_level_of_two_when_alert_for_running():
     depth = 60.0
     daily_average = 60.0
     running_average = 100.0
@@ -155,8 +52,7 @@ def test_calculate_alert__should_return_warning_level_of_two_when_alert_for_runn
     assert actual == 2
 
 
-@patch('svc.services.alert.send_alert')
-def test_calculate_alert__should_return_warning_level_of_one_when_warning_for_running(mock_alert):
+def test_calculate_alert__should_return_warning_level_of_one_when_warning_for_running():
     depth = 80.0
     daily_average = 80.0
     running_average = 100.0
@@ -165,8 +61,25 @@ def test_calculate_alert__should_return_warning_level_of_one_when_warning_for_ru
     assert actual == 1
 
 
-@patch('svc.services.alert.send_alert')
-def test_calculate_alert__should_return_warning_level_of_one_when_warning_for_daily(mock_alert):
+def test_calculate_alert__should_return_warning_level_of_one_when_daily_equals_alert_threshold():
+    depth = 80.0
+    daily_average = 96.0
+    running_average = 80.0
+    actual = calculate_alert(depth, daily_average, running_average)
+
+    assert actual == 1
+
+
+def test_calculate_alert__should_return_warning_level_of_one_when_running_equals_alert_threshold():
+    depth = 80.0
+    daily_average = 80.0
+    running_average = 96.0
+    actual = calculate_alert(depth, daily_average, running_average)
+
+    assert actual == 1
+
+
+def test_calculate_alert__should_return_warning_level_of_one_when_warning_for_daily():
     depth = 80.0
     daily_average = 100.0
     actual = calculate_alert(depth, daily_average, 80.0)
