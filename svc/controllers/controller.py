c1b8a1dee9769000da8f20b55a3aa71a81335692
Jon Graf <jgraf@vermeer.com>
2026-04-16 09:18:00 -0500

    updated the sump controller to send responses on an hourly basis, first call, or if the level is rising too rapidly or falling too rapidly below a certain threshold

--- svc/controllers/controller.py
+++ svc/controllers/controller.py
@@ -6,30 +6,52 @@ from svc.utilities.depth import get_depth_by_intervals
 from svc.utilities.gpio import get_intervals
 
 
+REPORT_INTERVAL = 3600
+DEPTH_THRESHOLD = 2.0
+
+
 class DepthController:
-    AVERAGE_DEPTH = 0
-    ITERATION = 0
+    def __init__(self):
+        self.average_depth = 0
+        self.iteration = 0
+        self.last_reported_depth = None
+        self.last_reported_alert = 0
+        self.last_reported_time = None
 
     def measure_depth(self):
         start, stop = get_intervals()
         current_depth = get_depth_by_intervals(start, stop)
         logging.info('Current depth: {}'.format(current_depth))
 
-        self.__update_average_depth(current_depth)
-        alert_level = calculate_alert(current_depth, self.__get_daily_average(), None)
-        api_requests.save_current_depth(current_depth, stop, alert_level)
+        self._update_average_depth(current_depth)
+        alert_level = calculate_alert(current_depth, self._get_daily_average(), None)
+
+        if self._should_report(current_depth, alert_level, stop):
+            api_requests.save_current_depth(current_depth, stop, alert_level)
+            self.last_reported_depth = current_depth
+            self.last_reported_alert = alert_level
+            self.last_reported_time = stop
 
         return current_depth
 
     def save_daily_average(self):
-        logging.info('Recording Daily Average depth: {}'.format(self.AVERAGE_DEPTH))
-        api_requests.save_daily_average_depth(self.AVERAGE_DEPTH)
-        self.AVERAGE_DEPTH = 0
-        self.ITERATION = 0
+        logging.info('Recording Daily Average depth: {}'.format(self.average_depth))
+        api_requests.save_daily_average_depth(self.average_depth)
+        self.average_depth = 0
+        self.iteration = 0
 
-    def __get_daily_average(self):
-        return 0 if self.ITERATION == 0 else self.AVERAGE_DEPTH / self.ITERATION
+    def _should_report(self, depth, alert_level, time):
+        if self.last_reported_depth is None:
+            return True
+        if alert_level != self.last_reported_alert:
+            return True
+        if time - self.last_reported_time >= REPORT_INTERVAL:
+            return True
+        return abs(depth - self.last_reported_depth) >= DEPTH_THRESHOLD
 
-    def __update_average_depth(self, depth):
-        self.AVERAGE_DEPTH += depth
-        self.ITERATION += 1
+    def _get_daily_average(self):
+        return 0 if self.iteration == 0 else self.average_depth / self.iteration
+
+    def _update_average_depth(self, depth):
+        self.average_depth += depth
+        self.iteration += 1
